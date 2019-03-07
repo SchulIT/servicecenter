3ac112d3c72de2bf6129d409c7e5172e6210aa63
Marcel Marnitz <marcelmarnitz@googlemail.com>
2019-03-07 17:59:20 +0100

    Fixes PDF export due to missing fonts

--- src/Helper/Placards/PdfExporter.php
+++ src/Helper/Placards/PdfExporter.php
@@ -7,32 +7,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class PdfExporter {
-    private $fontDirectory;
     private $version;
 
-    public function __construct($fontDirectory, $version) {
-        $this->fontDirectory = $fontDirectory;
+    public function __construct($version) {
         $this->version = $version;
     }
 
     public function getPdf(Placard $placard) {
-        if(!defined('FPDF_FONTPATH')) {
-            define('FPDF_FONTPATH', $this->fontDirectory);
-        }
-
         $pdf = new \FPDF('L', 'mm', 'A4');
         $pdf->SetCreator(sprintf('ServiceCenter %s', $this->version));
         $pdf->addPage();
         $pdf->SetAutoPageBreak(false);
 
-        $pdf->AddFont('SegoeUI','R', 'segoeui.php');
-        $pdf->AddFont('SegoeUI','B', 'segoeuib.php');
-
-        $pdf->setFont('SegoeUI', 'R', 32);
+        $pdf->setFont('Helvetica', '', 32);
         $pdf->Cell(30, 30, utf8_decode(sprintf('Anschlüsse %s (%s)', $placard->getHeader(), $placard->getRoom()->getName())), 0, 1);
 
         $pdf->setFillColor(204, 255, 204);
-        $pdf->setFont('SegoeUI', 'B', 22);
+        $pdf->setFont('Helvetica', 'B', 22);
         $pdf->Cell(92, 15, 'Signalquelle', 0, 0, 'C', true);
 
         $pdf->setFillColor(255, 255, 153);
@@ -41,7 +32,7 @@ class PdfExporter {
         $pdf->setFillColor(204, 255, 255);
         $pdf->Cell(92, 15, 'AV-Receiver', 0, 1, 'C', true);
 
-        $pdf->setFont('SegoeUI', 'R', 22);
+        $pdf->setFont('Helvetica', '', 22);
         foreach($placard->getDevices() as $device) {
             $pdf->setFillColor(204, 255, 204);
             $pdf->Cell(92, 20, utf8_decode($device->getSource()), 0, 0, 'C', true);
@@ -54,7 +45,7 @@ class PdfExporter {
         }
 
         $pdf->SetXY(10, -20);
-        $pdf->SetFont('SegoeUI', 'R', 12);
+        $pdf->SetFont('Helvetica', '', 12);
         $pdf->Cell(145, 12, utf8_decode(sprintf('%s, %s', $placard->getUpdatedBy()->getLastname(), $placard->getUpdatedBy()->getFirstname())));
         $pdf->Cell(132, 12, $placard->getUpdatedAt()->format('d.m.Y'), 0, 1, 'R');
 
