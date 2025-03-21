fdd54c3fc30fc8361728bf40ab83cf8f533aaa41
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-03-21 16:21:44 +0100

    Fixes application setup

--- src/Command/SetupCommand.php
+++ src/Command/SetupCommand.php
@@ -9,71 +9,39 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\PdoSessionHandler;
 
 #[AsCommand('app:setup', 'Installiert die Anwendung')]
 class SetupCommand extends Command {
-    public function __construct(private readonly EntityManagerInterface $em, ?string $name = null) {
+    public function __construct(private readonly EntityManagerInterface $em, private readonly PdoSessionHandler $pdoSessionHandler, ?string $name = null) {
         parent::__construct($name);
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int {
         $io = new SymfonyStyle($input, $output);
-
-        $io->section('Create session table...');
         try {
-            $this->setupSessions();
+            $this->setupSessions($io);
         } catch(Exception $e) {
             $this->getApplication()->renderThrowable($e, $output);
         }
-        $io->success('Session table created.');
 
-        $io->section('Create key-value store...');
-        try {
-            $this->setupKeyValueStore();
-        } catch (Exception $e) {
-            $this->getApplication()->renderThrowable($e, $output);
-            return 1;
-        }
-
-        $io->success('Key-value store created.');
-
-        $io->success('Setup completed');
         return 0;
     }
 
-    private function getEntityManager(): EntityManagerInterface {
-        return $this->em;
-    }
-
     /**
      * @throws \Doctrine\DBAL\Exception
      */
-    private function setupSessions(): void {
-        $sql = <<<SQL
-CREATE TABLE IF NOT EXISTS `sessions` (
-    `sess_id` VARCHAR(128) NOT NULL PRIMARY KEY,
-    `sess_data` BLOB NOT NULL,
-    `sess_time` INTEGER UNSIGNED NOT NULL,
-    `sess_lifetime` MEDIUMINT NOT NULL
-);
-SQL;
+    private function setupSessions(SymfonyStyle $style): void {
+        $style->section('Sessions-Tabelle');
 
-        $this->getEntityManager()->getConnection()->executeQuery($sql);
-    }
+        $sql = "SHOW TABLES LIKE 'sessions';";
+        $row = $this->em->getConnection()->executeQuery($sql);
 
-    /**
-     * @throws \Doctrine\DBAL\Exception
-     */
-    private function setupKeyValueStore(): void {
-        $sql = <<<SQL
-CREATE TABLE IF NOT EXISTS `idp_exchange` (
-      `id` INT NOT NULL AUTO_INCREMENT,
-      `meta_key` VARCHAR(255) NULL,
-      `meta_value` BLOB NULL,
-      PRIMARY KEY (`id`),
-      UNIQUE INDEX `meta_key_UNIQUE` (`meta_key` ASC)
-);
-SQL;
-        $this->getEntityManager()->getConnection()->executeQuery($sql);
+        if($row->fetchAssociative() === false) {
+            $this->pdoSessionHandler->createTable();
+            $style->success('Sessions-Tabelle erstellt');
+        } else {
+            $style->success('Sessions-Tabelle bereits vorhanden.');
+        }
     }
 }
