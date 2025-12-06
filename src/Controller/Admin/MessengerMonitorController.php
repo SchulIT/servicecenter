cd8675404e0a0ddf0096974f4988a669984be1f6
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-12-06 14:23:15 +0100

    Update dependencies for Symfony 7.4

---
+++ src/Controller/Admin/MessengerMonitorController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Zenstruck\Messenger\Monitor\Controller\MessengerMonitorController as BaseMessengerMonitorController;
+
+#[Route('/admin/messenger')]
+#[IsGranted('ROLE_SUPER_ADMIN')]
+final class MessengerMonitorController extends BaseMessengerMonitorController
+{
+}
